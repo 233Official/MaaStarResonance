d5ba32c6be179afe5ef5e2b71410a7636b7d01ed
233JG <ayusummer233@gmail.com>
2025-10-07 22:51:52 +0800

    🎨 目录结构调整

--- scripts/install.py
+++ scripts/install.py
@@ -4,10 +4,10 @@ import shutil
 import sys
 import json
 
-from configure import configure_ocr_model
+from scripts.configure import configure_ocr_model
 
 
-working_dir = Path(__file__).parent
+working_dir = (Path(__file__).parent / "..").resolve()
 install_path = working_dir / Path("install")
 version = len(sys.argv) > 1 and sys.argv[1] or "v0.0.1"
 
