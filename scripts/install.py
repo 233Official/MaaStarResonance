98c810d669f303b287cad191477eed44fcbe7a35
233Official <ayusummer233@outlook.com>
2025-11-10 02:38:48 +0800

    fix🐛: 尝试修复依赖安装报错问题(#9)

--- scripts/install.py
+++ scripts/install.py
@@ -137,6 +137,10 @@ def install_embed_python():
 
 # 拷贝 python wheels 以及 get-pip.py 和 pyproject.toml
 def copy_python_wheels():
+    # 目前只对 Windows x64 下载了 wheels
+    if platform != "win" or arch != "x64":
+        print(f"当前平台为 {platform} {arch}, 跳过 wheels 拷贝步骤")
+        return
     wheels_source_path = working_dir / "resource" / "wheels"
     wheels_target_path = install_path / "deps" / "wheels"
     shutil.copytree(wheels_source_path, wheels_target_path, dirs_exist_ok=True)
