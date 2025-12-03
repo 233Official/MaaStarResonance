296bb47e072ceec301d569a1ac558d0659f61756
azmiao <2362020227@qq.com>
2025-12-03 14:50:52 +0800

    fix🐛: 修复和优化包安装与引用，更新MaaFW至5.1.3

--- agent/main.py
+++ agent/main.py
@@ -1,16 +1,11 @@
+import importlib
 import subprocess
 import sys
 from pathlib import Path
 
-# 补充当前路径和上级路径
+# 获取：当前目录 / 项目根目录 / wheels目录 的绝对路径
 CURRENT_DIR = Path(__file__).parent.resolve()
-PARENT_DIR = CURRENT_DIR.parent
-if str(CURRENT_DIR) not in sys.path:
-    sys.path.insert(0, str(CURRENT_DIR))
-if str(PARENT_DIR) not in sys.path:
-    sys.path.insert(0, str(PARENT_DIR))
-
-PROJECT_ROOT = (Path(__file__).parent / "..").resolve()
+PROJECT_ROOT = CURRENT_DIR.parent
 WHEELS_DIR = PROJECT_ROOT / "deps" / "wheels"
 
 
@@ -18,6 +13,7 @@ def check_req_ready() -> bool:
     """通过尝试 import maa 来检查依赖是否安装完成"""
     try:
         import maa  # noqa: F401
+        _ = maa
 
         print("maa imported successfully")
         return True
@@ -28,6 +24,9 @@ def check_req_ready() -> bool:
 
 def init_python_env():
     """离线安装 pip, setuptools, wheel 以及项目依赖"""
+    print(f"===== 开始安装/更新 Python 依赖 =====")
+
+    # 检查 python 文件夹和可执行文件
     embed_python_path = PROJECT_ROOT / "python"
     if not embed_python_path.exists():
         print("请先运行 install.py 脚本安装 Python 运行环境")
@@ -40,19 +39,21 @@ def init_python_env():
         print("Cannot find Python executable, please check if the python folder is correct.")
         sys.exit(1)
 
-    # 安装 pip
+    # 检查 pip 安装脚本
     get_pip_script = PROJECT_ROOT / "deps" / "get-pip.py"
     if not get_pip_script.exists():
         print("无法找到 get-pip.py，请检查 deps 文件夹是否正确")
         print("Cannot find get-pip.py, please check if the deps folder is correct.")
         sys.exit(1)
 
+    # 安装 pip
     subprocess.check_call(
         [
             str(python_executable),
             str(get_pip_script),
             "--no-index",
             f"--find-links={WHEELS_DIR}",
+            "--no-warn-script-location"
         ]
     )
 
@@ -65,8 +66,9 @@ def init_python_env():
             "install",
             "--no-index",
             f"--find-links={WHEELS_DIR}",
+            "--no-warn-script-location",
             "setuptools",
-            "wheel",
+            "wheel"
         ]
     )
 
@@ -80,55 +82,52 @@ def init_python_env():
             "--no-index",
             f"--find-links={WHEELS_DIR}",
             "--no-build-isolation",
-            f"{PROJECT_ROOT}",
+            "--no-warn-script-location",
+            f"{PROJECT_ROOT}"
         ]
     )
 
-    # 安装完成后让当前进程也能看到刚写进的 site-packages
+    # 补充独立 python 环境的 site-packages 扫描路径
     site_packages = (PROJECT_ROOT / "python" / "Lib" / "site-packages").resolve()
     if site_packages.exists() and str(site_packages) not in sys.path:
         sys.path.insert(0, str(site_packages))
-    import importlib
+
+    # 强制刷新缓存 | 让当前进程也能扫描到刚安装的依赖
     importlib.invalidate_caches()
 
-    print("Python 依赖安装/更新 已完成\n")
+    print("===== Python 依赖安装/更新 已完成 =====")
 
 
 def main():
-    print(f"开始安装/更新 Python 依赖")
-
-    # 开发时应当注释下面两行, 编译时自动解除注释
-    # init_python_env()
+    # 开发时应当注释下面这行, 编译时自动解除注释
+    init_python_env()
 
     # 导入MAA工具
     from maa.agent.agent_server import AgentServer
     from maa.toolkit import Toolkit
 
-    # 导入整个 Agent 包
-    import agent
-    _ = agent
-
     # 导入基础包
     from agent.logger import logger
     from agent.module_loader import load_plugins
 
-    logger.info("===== 开始初始化MAA模块包 =====")
+    logger.info("===== 开始初始化MAA程序 =====")
 
-    # 加载 Agent 包下所有的文件夹
+    # 加载 agent 包下所有的模块
     for item in CURRENT_DIR.iterdir():
         # 跳过 __pycache__
         if item.is_dir() and item.name != "__pycache__":
             load_plugins(str(item), f"agent.{item.name}")
             logger.info(f"> 子模块 {item.name} 加载完成！")
 
-    logger.info("===== MAA模块包初始化完成 =====\n")
+    logger.info("===== MAA程序初始化完成 =====")
 
+    # 启动MAA主程序
     Toolkit.init_option("./")
-
-    socket_id = sys.argv[-1]
-    AgentServer.start_up(socket_id)
+    AgentServer.start_up(sys.argv[-1])
     AgentServer.join()
     AgentServer.shut_down()
+
+
 
 
 if __name__ == "__main__":
