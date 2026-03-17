52233f70f1fcb93804450cbcc775b4df7b38809d
azmiao <2362020227@qq.com>
2026-03-17 11:26:20 +0800

    feat✨: 新增开发版本的上下载具和开关自动战斗的函数

--- agent/custom/cocoon_action.py
+++ agent/custom/cocoon_action.py
@@ -6,6 +6,7 @@ from maa.custom_action import CustomAction
 
 from agent.attach.common_attach import get_need_cocoon_name
 from agent.constant.map_point import NAVIGATE_DATA
+from agent.custom.general.move_battle import mount_vehicle, auto_attach
 from agent.custom.general.power_saving_mode import exit_power_saving_mode
 from agent.custom.general.world_line_switcher import switch_line
 from agent.custom.teleport_action import teleport_or_navigate
@@ -36,8 +37,8 @@ class CocoonActionAction(CustomAction):
         cocoon_name = get_need_cocoon_name(context)
         # 传送到目的位置
         teleport_or_navigate(context, None, cocoon_name, "导航", NAVIGATE_DATA)
-        # TODO 点击按钮下马
-        logger.debug("点击按钮下马代码占位符")
+        # 点击按钮下马
+        mount_vehicle(context, mount_type=0)
 
         # 确保到达茧的入口
         need_next = ensure_cocoon_entry(context)
@@ -47,8 +48,8 @@ class CocoonActionAction(CustomAction):
         # 尝试切换到一条靠前的分线
         switch_line(context, ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
 
-        # TODO 确保自动战斗关闭
-        logger.debug("自动战斗关闭代码占位符")
+        # 确保自动战斗关闭
+        auto_attach(context, attack_type=0)
 
         # 点击进入茧
         context.tasker.controller.post_click(0, 0)  # TODO 进茧按钮坐标
@@ -74,12 +75,10 @@ class CocoonActionAction(CustomAction):
 
             if ocr_result.hit:
                 # 有幻觉值，需要确保自动战斗还开着
-                # TODO 确保自动战斗开启
-                logger.debug("自动战斗开启代码占位符")
+                auto_attach(context, attack_type=1)
             else:
                 # 没有幻觉值，需要关闭自动战斗后识别进茧再开自动战斗
-                # TODO 确保自动战斗关闭
-                logger.debug("自动战斗关闭代码占位符")
+                auto_attach(context, attack_type=0)
                 # 检测还有没有按钮
                 has_entry = ensure_cocoon_entry(context, 10)
                 if has_entry:
@@ -95,8 +94,8 @@ class CocoonActionAction(CustomAction):
                     need_next = ensure_cocoon_entry(context)
                     if not need_next:
                         return False
-                    # TODO 点击按钮下马
-                    logger.debug("点击按钮下马代码占位符")
+                    # 击按钮下马
+                    mount_vehicle(context, mount_type=0)
                     # 点击进入茧
                     context.tasker.controller.post_click(0, 0)  # TODO 进茧按钮坐标
             # 10秒检查一次
---
+++ agent/custom/general/move_battle.py
@@ -0,0 +1,72 @@
+import time
+
+import numpy
+from maa.context import Context, RecognitionDetail
+
+from agent.logger import logger
+
+
+def mount_vehicle(context: Context, mount_type: int = 0) -> bool:
+    """
+    如果识别到上/下载具按钮就上/下载具 | 其实如果不想识别直接按下 G 就行
+
+    Args:
+        context: 控制器上下文
+        mount_type: 类型：0下载具，1上载具
+
+    Returns: 是否成功
+
+    """
+    # 首次识别
+    img: numpy.ndarray = context.tasker.controller.post_screencap().wait().get()
+    entry = "图片识别上载具图标" if mount_type else "图片识别下载具图标"
+    detail: RecognitionDetail | None = context.run_recognition(entry, img)  # TODO 上下载具图标绿幕切片
+    if detail and detail.hit:
+        context.tasker.controller.post_click(0, 0).wait()  # TODO 点击上下载具
+        return True
+    # 第一次未识别到：可能是在战斗技能页面 | 点击按钮切换页面
+    time.sleep(1)
+    context.tasker.controller.post_click(0, 0).wait()  # TODO 点击切换页面
+    # 再次识别
+    time.sleep(1)
+    img: numpy.ndarray = context.tasker.controller.post_screencap().wait().get()
+    detail: RecognitionDetail | None = context.run_recognition(entry, img)  # TODO 上下载具图标绿幕切片
+    if detail and detail.hit:
+        context.tasker.controller.post_click(0, 0).wait()  # TODO 点击上下载具
+        return True
+    # 确实没识别到
+    logger.error("未识别到上/下载具的图标")
+    return False
+
+
+def auto_attach(context: Context, attack_type: int = 0) -> bool:
+    """
+    如果识别到开/关自动战斗按钮就开/关自动战斗 | 其实如果不想识别直接按下 H 就行
+
+    Args:
+        context: 控制器上下文
+        attack_type: 类型：0关自动战斗，1开自动战斗
+
+    Returns: 是否成功
+
+    """
+    # 首次识别
+    img: numpy.ndarray = context.tasker.controller.post_screencap().wait().get()
+    entry = "图片识别开自动战斗" if attack_type else "图片识别关自动战斗"
+    detail: RecognitionDetail | None = context.run_recognition(entry, img)  # TODO 开关自动战斗图标绿幕切片
+    if detail and detail.hit:
+        context.tasker.controller.post_click(0, 0).wait()  # TODO 点击开关自动战斗
+        return True
+    # 第一次未识别到：可能是在战斗技能页面 | 点击按钮切换页面
+    time.sleep(1)
+    context.tasker.controller.post_click(0, 0).wait()  # TODO 点击切换页面
+    # 再次识别
+    time.sleep(1)
+    img: numpy.ndarray = context.tasker.controller.post_screencap().wait().get()
+    detail: RecognitionDetail | None = context.run_recognition(entry, img)  # TODO 开关自动战斗图标绿幕切片
+    if detail and detail.hit:
+        context.tasker.controller.post_click(0, 0).wait()  # TODO 点击开关自动战斗
+        return True
+    # 确实没识别到
+    logger.error("未识别到开/关自动战斗的图标")
+    return False
